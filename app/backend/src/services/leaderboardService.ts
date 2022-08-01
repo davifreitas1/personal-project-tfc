de9e2fedf6dfe4d02356e87f9f86aa80e94ef5ff
unknown <davivarejao@gmail.com>
2022-08-01 16:54:51 -0300

    incrementa leaderboardService e processData

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -14,17 +14,55 @@ class LeaderboardService {
     this._leader = this._leader.sort((a, b) => a.goalsOwn - b.goalsOwn);
     this._leader = this._leader.sort((a, b) => b.goalsFavor - a.goalsFavor);
     this._leader = this._leader.sort((a, b) => b.goalsBalance - a.goalsBalance);
-    this._leader = this._leader.sort((a, b) => b.totalVictories - a.totalVictories);
+    this._leader = this._leader.sort((a, b) => b.totalPoints - a.totalPoints);
   }
 
-  private async teamsMatches() {
+  private async getTeamsAndMatches() {
     this._teams = await this.teamsModel.findAll();
     this._matches = await this.matchesModel.findAll({ where: { inProgress: false } });
+  }
 
+  private teamsMatchesHome() {
     this._leader = this._teams.map((team) => {
       const { teamName } = team;
-      const teamMatches = this._matches.filter((match) => match.homeTeam === team.id);
-      const data = new ProcessData(teamMatches);
+      const teamMatches = this._matches.filter((match) => (
+        match.homeTeam === team.id || match.awayTeam === team.id
+      ));
+      const data = new ProcessData(teamMatches, team.id);
+      const matchesData = data.homeData();
+      const result = {
+        ...matchesData,
+        name: teamName,
+      };
+      return result;
+    });
+    this.sort();
+  }
+
+  private teamsMatchesAway() {
+    this._leader = this._teams.map((team) => {
+      const { teamName } = team;
+      const teamMatches = this._matches.filter((match) => (
+        match.homeTeam === team.id || match.awayTeam === team.id
+      ));
+      const data = new ProcessData(teamMatches, team.id);
+      const matchesData = data.awayData();
+      const result = {
+        ...matchesData,
+        name: teamName,
+      };
+      return result;
+    });
+    this.sort();
+  }
+
+  private teamsMatchesTotal() {
+    this._leader = this._teams.map((team) => {
+      const { teamName } = team;
+      const teamMatches = this._matches.filter((match) => (
+        match.homeTeam === team.id || match.awayTeam === team.id
+      ));
+      const data = new ProcessData(teamMatches, team.id);
       const matchesData = data.totalData();
       const result = {
         ...matchesData,
@@ -32,12 +70,24 @@ class LeaderboardService {
       };
       return result;
     });
-
     this.sort();
   }
 
   async home() {
-    await this.teamsMatches();
+    await this.getTeamsAndMatches();
+    this.teamsMatchesHome();
+    return this._leader;
+  }
+
+  async away() {
+    await this.getTeamsAndMatches();
+    this.teamsMatchesAway();
+    return this._leader;
+  }
+
+  async total() {
+    await this.getTeamsAndMatches();
+    this.teamsMatchesTotal();
     return this._leader;
   }
 }
--- app/backend/src/services/processData.ts
+++ app/backend/src/services/processData.ts
@@ -1,6 +1,7 @@
 import Matches from '../database/models/Matches';
 
 class ProcessData {
+  private _id: number;
   private _matches: Matches[];
   private _totalPoints: number;
   private _totalGames: number;
@@ -12,7 +13,8 @@ class ProcessData {
   private _goalsBalance: number;
   private _efficiency: number;
 
-  constructor(matches: Matches[]) {
+  constructor(matches: Matches[], id: number) {
+    this._id = id;
     this._matches = matches;
     this._totalPoints = 0;
     this._totalGames = 0;
@@ -23,33 +25,48 @@ class ProcessData {
     this._goalsOwn = 0;
     this._goalsBalance = 0;
     this._efficiency = 0;
-    this.calculate();
   }
 
-  private calculate() {
-    this._totalGames = this._matches.length;
+  private calculateHome() {
+    const inHomeGames = this._matches.filter((match) => match.homeTeam === this._id);
+    this._totalGames += inHomeGames.length;
 
-    for (let index = 0; index < this._matches.length; index += 1) {
-      const { homeTeamGoals, awayTeamGoals } = this._matches[index];
+    for (let index = 0; index < inHomeGames.length; index += 1) {
+      const { homeTeamGoals, awayTeamGoals } = inHomeGames[index];
 
       this._goalsFavor += homeTeamGoals;
       this._goalsOwn += awayTeamGoals;
       this.getMatchResult(homeTeamGoals, awayTeamGoals);
     }
-
-    this._goalsBalance = this._goalsFavor - this._goalsOwn;
-    this._totalPoints = (this._totalVictories * 3) + this._totalDraws;
-    this._efficiency = Number(((this._totalPoints / (this._totalGames * 3)) * 100).toFixed(2));
   }
 
-  private getMatchResult(homeTeamGoals: number, awayTeamGoals: number) {
-    if (homeTeamGoals > awayTeamGoals) {
+  private calculateAway() {
+    const inAwayGames = this._matches.filter((match) => match.awayTeam === this._id);
+    this._totalGames += inAwayGames.length;
+
+    for (let index = 0; index < inAwayGames.length; index += 1) {
+      const { homeTeamGoals, awayTeamGoals } = inAwayGames[index];
+
+      this._goalsFavor += awayTeamGoals;
+      this._goalsOwn += homeTeamGoals;
+      this.getMatchResult(awayTeamGoals, homeTeamGoals);
+    }
+  }
+
+  private getMatchResult(goalsFavor: number, goalsOwn: number) {
+    if (goalsFavor > goalsOwn) {
       this._totalVictories += 1;
-    } else if (homeTeamGoals < awayTeamGoals) {
+    } else if (goalsFavor < goalsOwn) {
       this._totalLosses += 1;
     } else {
       this._totalDraws += 1;
     }
+  }
+
+  private finalCalculation() {
+    this._goalsBalance = this._goalsFavor - this._goalsOwn;
+    this._totalPoints = (this._totalVictories * 3) + this._totalDraws;
+    this._efficiency = Number(((this._totalPoints / (this._totalGames * 3)) * 100).toFixed(2));
   }
 
   private goalsData() {
@@ -80,7 +97,26 @@ class ProcessData {
     };
   }
 
+  homeData() {
+    this.calculateHome();
+    this.finalCalculation();
+    const points = this.pointsData();
+    const goals = this.goalsData();
+    return { ...goals, ...points };
+  }
+
+  awayData() {
+    this.calculateAway();
+    this.finalCalculation();
+    const points = this.pointsData();
+    const goals = this.goalsData();
+    return { ...goals, ...points };
+  }
+
   totalData() {
+    this.calculateHome();
+    this.calculateAway();
+    this.finalCalculation();
     const points = this.pointsData();
     const goals = this.goalsData();
     return { ...goals, ...points };
